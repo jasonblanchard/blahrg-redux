2935c75c68db7c75d898e06c2618e09399de6d69
Jason Blanchard <jas.blanchard@gmail.com>
2015-09-01 09:37:29 -0700

    Moved reducers to an object

--- src/shared/reducers/blog_reducer.js
+++ src/shared/reducers/blog_reducer.js
@@ -31,75 +31,71 @@ const { SHOW_ALL } = VisibilityFilters;
  *
  */
 
-function visibilityFilter(state = SHOW_ALL, action) {
-  switch(action.type) {
-  case SET_VISIBILITY_FILTER:
-    return action.filter;
-  default:
-    return state;
+const reducers = {
+  visibilityFilter(state = SHOW_ALL, action) {
+    switch(action.type) {
+      case SET_VISIBILITY_FILTER:
+        return action.filter;
+      default:
+        return state;
+    }
+  },
+
+  tagFilter(state = [], action) {
+    switch(action.type) {
+    case SET_TAG_FILTER:
+      return action.tagIds;
+    default:
+      return state;
+    }
+  },
+
+  pendingPost(state = {}, action) {
+    switch(action.type) {
+    case UPDATE_PENDING_POST:
+      return action.postParams;
+    default:
+      return state
+    }
+  },
+
+  tags(state = [], action) {
+    switch(action.type) {
+    default:
+      return state;
+    }
+  },
+
+  posts(state = [], action) {
+    switch(action.type) {
+    case ADD_POST:
+      let postParams = action.postParams
+      return [...state, {
+        id: postParams.id,
+        title: postParams.title,
+        body: postParams.body,
+        featured: postParams.featured,
+        tags: []
+      }]
+    case FEATURE_POST:
+      
+      let index = state.findIndex((element, index) => {
+        return element.id === action.id;
+      });
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, state[index], {
+          featured: !state[index].featured
+        }),
+        ...state.slice(index + 1)
+      ]
+    default:
+      return state;
+    }
   }
 }
 
-function tagFilter(state = [], action) {
-  switch(action.type) {
-  case SET_TAG_FILTER:
-    return action.tagIds;
-  default:
-    return state;
-  }
-}
-
-function pendingPost(state = {}, action) {
-  switch(action.type) {
-  case UPDATE_PENDING_POST:
-    return action.postParams;
-  default:
-    return state
-  }
-}
-
-function tags(state = [], action) {
-  switch(action.type) {
-  default:
-    return state;
-  }
-}
-
-function posts(state = [], action) {
-  switch(action.type) {
-  case ADD_POST:
-    let postParams = action.postParams
-    return [...state, {
-      id: postParams.id,
-      title: postParams.title,
-      body: postParams.body,
-      featured: postParams.featured,
-      tags: []
-    }]
-  case FEATURE_POST:
-    
-    let index = state.findIndex((element, index) => {
-      return element.id === action.id;
-    });
-
-    return [
-      ...state.slice(0, index),
-      Object.assign({}, state[index], {
-        featured: !state[index].featured
-      }),
-      ...state.slice(index + 1)
-    ]
-  default:
-    return state;
-  }
-}
-
-const blogApp = combineReducers({
-  visibilityFilter,
-  tagFilter,
-  posts,
-  tags,
-  pendingPost
-});
+const blogApp = combineReducers(reducers);
 
 export default blogApp;
